1d25b7affce4bea5dc138bc91b7c76e8ac4e2b72
BatmanMurillo <sebastian.espinoza0268@alumnos.udg.mx>
2025-11-06 16:12:44 -0600

    subir proyecto

---
+++ app.js
@@ -0,0 +1,10 @@
+// Agregar el evento para abrir/cerrar el menú lateral
+const menuButton = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const body = document.body;
+
+// Agregar un evento para mostrar/ocultar el menú
+menuButton.addEventListener("click", function() {
+    menu.classList.toggle("show"); // Alterna la clase 'show' para mostrar/ocultar el menú
+    body.classList.toggle("menu-active"); // Activa/desactiva el desplazamiento del contenido
+});
